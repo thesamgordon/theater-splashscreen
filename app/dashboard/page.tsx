3d619c676bae09e5dc6ec3dc12472973f6e35439
Sam Gordon <sam@thesamgordon.com>
2026-04-29 13:45:55 -0400

    feat: connection status

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,26 +12,42 @@ export default function Dashboard() {
 
   const [textInput, setTextInput] = useState("");
   const [hasSetConfig, setHasSetConfig] = useState(false);
+  const [lastUpdated, setLastUpdated] = useState(new Date());
+  const [now, setNow] = useState(new Date());
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("/api/state");
+      const json = await res.json();
+      setServerState(json);
+      setLastUpdated(new Date());
+
+      if (json.config && !inputFocus) {
+        setConfiguration(json.config);
+
+        if (!hasSetConfig) {
+          setTextInput(json.config.splash);
+          setHasSetConfig(true);
+        }
+      }
+    } catch (e) {}
+  };
 
   useEffect(() => {
-    const poll = setInterval(async () => {
-      try {
-        const res = await fetch("/api/state");
-        const json = await res.json();
-        setServerState(json);
+    queueMicrotask(() => {
+      fetchData();
+    });
 
-        if (json.config && !inputFocus) {
-          setConfiguration(json.config);
-
-          if (!hasSetConfig) {
-            setTextInput(json.config.splash);
-            setHasSetConfig(true);
-          }
-        }
-      } catch (e) {}
+    setInterval(() => {
+      setNow(new Date());
     }, 500);
+
+    const poll = setInterval(async () => {
+      await fetchData();
+    }, 500);
+
     return () => clearInterval(poll);
-  }, [inputFocus, hasSetConfig]);
+  }, []);
 
   const updateState = (payload: object) => {
     fetch("/api/state", {
@@ -51,247 +67,275 @@ export default function Dashboard() {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.statusBar}>
-        <div className={styles.top}>
-          <div className={styles.statusItem}>
-            <span>CURRENT VIEW</span>
-            <strong
-              className={
-                serverState.view === "intermission" ? styles.activeText : ""
-              }
-            >
-              {serverState.view.toUpperCase()}
+    <div className={styles.fullWrapper}>
+      <div className={styles.header}>
+        <div className={`${styles.statusItem} ${styles.right} ${hasSetConfig ? "" : styles.loading}`}>
+          <span>CURRENT VIEW</span>
+          <strong
+            className={
+              serverState.view === "intermission" ? styles.activeText : ""
+            }
+          >
+            {serverState.view.toUpperCase()}
+          </strong>
+        </div>
+
+        {serverState.view === "intermission" && (
+          <div className={`${styles.statusItem} ${styles.timer}`}>
+            <span>LOBBY CLOCK</span>
+            <strong className={styles.clockText}>
+              {formatTime(serverState.seconds)}
             </strong>
           </div>
-          {serverState.view === "intermission" && (
-            <div className={`${styles.statusItem} ${styles.timer}`}>
-              <span>LOBBY CLOCK</span>
-              <strong className={styles.clockText}>
-                {formatTime(serverState.seconds)}
-              </strong>
-            </div>
+        )}
+
+        <div className={styles.statusContainer}>
+          <span>CONNECTION STATUS</span>
+
+          <span
+            className={`${
+              now.getTime() - lastUpdated.getTime() < 1000
+                ? styles.connectionText
+                : styles.disconnectedText
+            }`}
+          >
+            {now.getTime() - lastUpdated.getTime() < 1000
+              ? "CONNECTED"
+              : "DISCONNECTED"}
+          </span>
+          {now.getTime() - lastUpdated.getTime() > 1000 && (
+            <span className={styles.secondaryText}>
+              Last updated{" "}
+              {lastUpdated &&
+              Math.max(now.getTime() - lastUpdated.getTime(), 0) < 1000
+                ? "just now."
+                : `${Math.floor((now.getTime() - lastUpdated.getTime()) / 1000)}s ago`}
+            </span>
           )}
         </div>
       </div>
 
-      <div className={styles.section}>
-        <h3>Scene Control</h3>
-        <div className={styles.grid}>
-          <button
-            className={`${styles.btn} ${styles.warn} ${serverState.view == "splash" ? styles.disabled : ""}`}
-            onClick={() => updateState({ view: "splash", timerActive: false })}
-          >
-            ACTIVATE SPLASH
-          </button>
-          <button
-            className={`${styles.btn} ${styles.warn} ${serverState.view == "live" ? styles.disabled : ""}`}
-            onClick={() =>
-              updateState({
-                view: "live",
-              })
-            }
-          >
-            GO LIVE
-          </button>
-          <button
-            className={`${styles.btn} ${styles.warn} ${serverState.view == "intermission" ? styles.disabled : ""}`}
-            onClick={() =>
-              updateState({
-                view: "intermission",
-                timerActive: true,
-                action: "set",
-                value: configuration.intermissionLength * 60,
-              })
-            }
-          >
-            START INTERMISSION
-          </button>
-          <button
-            className={`${styles.btn} ${styles.warn} ${serverState.view != "intermission" ? styles.disabled : ""}`}
-            onClick={() =>
-              updateState({
-                view: "intermission",
-                timerActive: false,
-                action: "set",
-                value: 0,
-              })
-            }
-          >
-            END INTERMISSION
-          </button>
-        </div>
-        <div className={styles.input}>
-          <input
-            type="text"
-            placeholder="Custom lobby text"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                setConfiguration({
-                  ...configuration,
-                  splash: textInput,
-                });
-                updateState({
-                  action: "updateConfig",
-                  value: { splash: textInput },
-                });
-
-                setInputFocus(false);
-                (e.target as HTMLInputElement).blur();
+      <div className={styles.wrapper}>
+        <div className={styles.section}>
+          <h3>Scene Control</h3>
+          <div className={styles.grid}>
+            <button
+              className={`${styles.btn} ${styles.warn} ${serverState.view == "splash" ? styles.disabled : ""}`}
+              onClick={() =>
+                updateState({ view: "splash", timerActive: false })
               }
-            }}
-            onFocus={() => setInputFocus(true)}
-            onBlur={() => setInputFocus(false)}
-            value={textInput}
-            onChange={(e) => setTextInput(e.target.value)}
-          />
-          <div>
+            >
+              ACTIVATE SPLASH
+            </button>
             <button
-              className={`${styles.btn} ${styles.submitBtn}`}
-              onClick={() => {
-                setConfiguration({
-                  ...configuration,
-                  splash: textInput,
-                });
+              className={`${styles.btn} ${styles.warn} ${serverState.view == "live" ? styles.disabled : ""}`}
+              onClick={() =>
                 updateState({
-                  action: "updateConfig",
-                  value: { splash: textInput },
-                });
-              }}
+                  view: "live",
+                })
+              }
             >
-              SET SPLASH
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.section}>
-        <h3>Timer Control</h3>
-        <div className={styles.timerControls}>
-          <div className={styles.adjustButtons}>
-            <button
-              className={styles.btn}
-              onClick={() => updateState({ action: "adjust", value: 60 })}
-            >
-              +1 MIN
+              GO LIVE
             </button>
             <button
-              className={styles.btn}
-              onClick={() => updateState({ action: "adjust", value: -60 })}
+              className={`${styles.btn} ${styles.warn} ${serverState.view == "intermission" ? styles.disabled : ""}`}
+              onClick={() =>
+                updateState({
+                  view: "intermission",
+                  timerActive: true,
+                  action: "set",
+                  value: configuration.intermissionLength * 60,
+                })
+              }
             >
-              -1 MIN
+              START INTERMISSION
+            </button>
+            <button
+              className={`${styles.btn} ${styles.warn} ${serverState.view != "intermission" ? styles.disabled : ""}`}
+              onClick={() =>
+                updateState({
+                  view: "intermission",
+                  timerActive: false,
+                  action: "set",
+                  value: 0,
+                })
+              }
+            >
+              END INTERMISSION
             </button>
           </div>
-
           <div className={styles.input}>
             <input
-              type="number"
-              placeholder="15"
-              value={customTime}
-              onChange={(e) => setCustomTime(parseInt(e.target.value))}
+              type="text"
+              placeholder="Custom lobby text"
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
-                  updateState({ action: "set", value: customTime * 60 });
+                  setConfiguration({
+                    ...configuration,
+                    splash: textInput,
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { splash: textInput },
+                  });
 
                   setInputFocus(false);
                   (e.target as HTMLInputElement).blur();
                 }
               }}
+              onFocus={() => setInputFocus(true)}
+              onBlur={() => setInputFocus(false)}
+              value={textInput}
+              onChange={(e) => setTextInput(e.target.value)}
             />
             <div>
               <button
                 className={`${styles.btn} ${styles.submitBtn}`}
                 onClick={() => {
-                  updateState({ action: "set", value: customTime * 60 });
+                  setConfiguration({
+                    ...configuration,
+                    splash: textInput,
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { splash: textInput },
+                  });
                 }}
               >
-                SET TIME
+                SET SPLASH
               </button>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.section}>
-        <h3>Show Configuration</h3>
-        <div className={styles.flex}>
-          <div className={styles.configItem}>
-            <label>Show Name</label>
-            <input
-              type="text"
-              value={configuration.showName}
-              onFocus={() => setInputFocus(true)}
-              onBlur={() => setInputFocus(false)}
-              onChange={(e) => {
-                setConfiguration({
-                  ...configuration,
-                  showName: e.target.value,
-                });
-                updateState({
-                  action: "updateConfig",
-                  value: { showName: e.target.value },
-                });
-              }}
-            />
+        <div className={styles.section}>
+          <h3>Timer Control</h3>
+          <div className={styles.timerControls}>
+            <div className={styles.adjustButtons}>
+              <button
+                className={styles.btn}
+                onClick={() => updateState({ action: "adjust", value: 60 })}
+              >
+                +1 MIN
+              </button>
+              <button
+                className={styles.btn}
+                onClick={() => updateState({ action: "adjust", value: -60 })}
+              >
+                -1 MIN
+              </button>
+            </div>
+
+            <div className={styles.input}>
+              <input
+                type="number"
+                placeholder="15"
+                value={customTime}
+                onChange={(e) => setCustomTime(parseInt(e.target.value))}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    updateState({ action: "set", value: customTime * 60 });
+
+                    setInputFocus(false);
+                    (e.target as HTMLInputElement).blur();
+                  }
+                }}
+              />
+              <div>
+                <button
+                  className={`${styles.btn} ${styles.submitBtn}`}
+                  onClick={() => {
+                    updateState({ action: "set", value: customTime * 60 });
+                  }}
+                >
+                  SET TIME
+                </button>
+              </div>
+            </div>
           </div>
-          <div className={styles.configItem}>
-            <label>Intermission Length (minutes)</label>
-            <input
-              type="number"
-              value={configuration.intermissionLength}
-              onFocus={() => setInputFocus(true)}
-              onBlur={() => setInputFocus(false)}
-              onChange={(e) => {
-                setConfiguration({
-                  ...configuration,
-                  intermissionLength: parseInt(e.target.value),
-                });
-                updateState({
-                  action: "updateConfig",
-                  value: { intermissionLength: parseInt(e.target.value) },
-                });
-              }}
-            />
-          </div>
-          <div className={styles.configItem}>
-            <label>Primary Color</label>
-            <input
-              type="color"
-              value={configuration.primaryColor}
-              onFocus={() => setInputFocus(true)}
-              onBlur={() => setInputFocus(false)}
-              className={styles.colorInput}
-              onChange={(e) => {
-                setConfiguration({
-                  ...configuration,
-                  primaryColor: e.target.value,
-                });
-                updateState({
-                  action: "updateConfig",
-                  value: { primaryColor: e.target.value },
-                });
-              }}
-            />
-          </div>
-          <div className={styles.configItem}>
-            <label>Secondary Color</label>
-            <input
-              type="color"
-              value={configuration.secondaryColor}
-              onFocus={() => setInputFocus(true)}
-              onBlur={() => setInputFocus(false)}
-              className={styles.colorInput}
-              onChange={(e) => {
-                setConfiguration({
-                  ...configuration,
-                  secondaryColor: e.target.value,
-                });
-                updateState({
-                  action: "updateConfig",
-                  value: { secondaryColor: e.target.value },
-                });
-              }}
-            />
+        </div>
+
+        <div className={styles.section}>
+          <h3>Show Configuration</h3>
+          <div className={styles.flex}>
+            <div className={styles.configItem}>
+              <label>Show Name</label>
+              <input
+                type="text"
+                value={configuration.showName}
+                onFocus={() => setInputFocus(true)}
+                onBlur={() => setInputFocus(false)}
+                onChange={(e) => {
+                  setConfiguration({
+                    ...configuration,
+                    showName: e.target.value,
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { showName: e.target.value },
+                  });
+                }}
+              />
+            </div>
+            <div className={styles.configItem}>
+              <label>Intermission Length (minutes)</label>
+              <input
+                type="number"
+                value={configuration.intermissionLength}
+                onFocus={() => setInputFocus(true)}
+                onBlur={() => setInputFocus(false)}
+                onChange={(e) => {
+                  setConfiguration({
+                    ...configuration,
+                    intermissionLength: parseInt(e.target.value),
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { intermissionLength: parseInt(e.target.value) },
+                  });
+                }}
+              />
+            </div>
+            <div className={styles.configItem}>
+              <label>Primary Color</label>
+              <input
+                type="color"
+                value={configuration.primaryColor}
+                onFocus={() => setInputFocus(true)}
+                onBlur={() => setInputFocus(false)}
+                className={styles.colorInput}
+                onChange={(e) => {
+                  setConfiguration({
+                    ...configuration,
+                    primaryColor: e.target.value,
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { primaryColor: e.target.value },
+                  });
+                }}
+              />
+            </div>
+            <div className={styles.configItem}>
+              <label>Secondary Color</label>
+              <input
+                type="color"
+                value={configuration.secondaryColor}
+                onFocus={() => setInputFocus(true)}
+                onBlur={() => setInputFocus(false)}
+                className={styles.colorInput}
+                onChange={(e) => {
+                  setConfiguration({
+                    ...configuration,
+                    secondaryColor: e.target.value,
+                  });
+                  updateState({
+                    action: "updateConfig",
+                    value: { secondaryColor: e.target.value },
+                  });
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
